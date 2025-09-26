9f014772f064153a80924bb9f39ce9def91be4f1
seer1314 <a0923552212@gmail.com>
2025-09-26 11:53:21 +0800

    v1

--- IM411070355/sqlctrl.py
+++ IM411070355/sqlctrl.py
@@ -1,5 +1,15 @@
 import sqlite3
 
+#INSERT
+sqlins ="""
+INSERT INTO "main"."product_info" ("name", "version", "remark")
+VALUES ('iPhone', '17.42', 'iPhone lover');
+"""
+cursor.execute(sqlins)
+cursor.fetchall()
+conn.commit()
+
+#SELECT
 conn = sqlite3.connect('DevOps.db')
 cursor = conn.cursor()
 cursor.execute('SELECT * FROM product_info')
